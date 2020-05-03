b6a5a0350f9245ab5188409d4a4a1f6ffb142f5a
我nalomu <2556468180@qq.com>
2020-05-03 12:51:22 +0900

    update README.MD

--- config.py
+++ config.py
@@ -11,8 +11,8 @@ NICKNAME = file_config.nickname
 ROOT = path.dirname(path.realpath(__file__))
 BOT_SWITCH = True
 APP_ROOT = path.abspath(path.dirname(__file__))
-DATA_ROOT = path.join(APP_ROOT, 'nalomu/data')
-TEMPLATES_ROOT = path.join(APP_ROOT, 'nalomu/templates')
+DATA_ROOT = path.join(APP_ROOT, 'nalomu','data')
+TEMPLATES_ROOT = path.join(APP_ROOT, 'nalomu','templates')
 if file_config.using_docker:
     DATA_URL = f'http://{file_config.server.host}:{file_config.server.port}/data'
 else:
